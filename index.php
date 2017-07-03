9459374b25f34a3a4eb1887ca2f24caa0b859795
WEBFUSION\jar <john.anderson@godaddy.com>
2017-07-03 11:30:08 +0100

    First code commit.

---
+++ index.php
@@ -0,0 +1,58 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+ 	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  	<title>Soaring Penguin Press</title>
+  	<meta name="description" content="Soaring Penguin Press">
+  	<meta name="author" content="Soaring Penguin|John Anderson">
+
+    <link href='https://fonts.googleapis.com/css?family=Goudy+Bookletter+1911|Lora' rel='stylesheet' type='text/css'>  	
+  	<link rel="stylesheet" href="css/style.css?v=1.0">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+  <style>
+
+  </style>
+</head>
+
+<body>
+	<div class="container">
+		<header>
+
+		</header>
+		<main>
+			<div class="row">
+				<article class="menu">
+					&nbsp;
+				</article>
+				<article>
+					&nbsp;
+				</article>	
+				<article>
+					&nbsp;
+				</article>
+				<article>
+					&nbsp;
+				</article>	
+				<article>
+					&nbsp;
+				</article>
+				<article>
+					&nbsp;
+				</article>					
+			</div>
+		</main>
+		<footer>
+		</footer>
+	</div>
+
+
+  	<script src="js/scripts.js"></script>
+</body>
+</html>
